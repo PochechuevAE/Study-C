411f124c975e2f87f17069c2802f391f82126d53
Pochechuev <trakturus@list.ru>
2023-07-30 13:49:01 +0300

    Example003_Summ_Random

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 31;
-int numberB = 5;
+int numberA = new Random().Next(1, 10); // 1, 2, 3, 4, 5, 6, 7, 8, 9
+Console.WriteLine("Первое число " + numberA );
+int numberB = new Random().Next(1, 10); //
+Console.WriteLine("Второе число " + numberB );
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine("Результат сложения = " + result);
 
