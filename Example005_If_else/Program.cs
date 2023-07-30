964458965a5740c9389ae6b285bf0d7faad6df07
Pochechuev <trakturus@list.ru>
2023-07-30 14:01:50 +0300

    Example005_If_else

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, Это же МАША!");
+}
+else 
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
