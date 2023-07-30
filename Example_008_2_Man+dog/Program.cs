563b0176dc2e117b805cfdeea0a55bf6b03166d8
Pochechuev <trakturus@list.ru>
2023-07-30 15:10:36 +0300

    Example008_2_Man+dog

---
+++ Example_008_2_Man+dog/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int distance = 10000;
+int firsfs = 1;
+int secondfs = 2;
+int dogs = 5;
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1) 
+    {
+        time = distance / (firsfs + dogs);
+        friend = 2;
+    }
+    else 
+    {
+        time = distance / (secondfs + dogs);
+        friend = 1;
+    }
+    distance = distance - time * (firsfs + secondfs);
+    count++;
+}
+Console.WriteLine("Собака пробежит " + count + " раз");
