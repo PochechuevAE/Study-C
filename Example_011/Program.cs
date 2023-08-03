034711730d64a46ec0e309bf3baea075bf6a760c
Pochechuev <trakturus@list.ru>
2023-08-03 21:23:43 +0300

    Seminar 2 Example_011

---
+++ Example_011/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int ostatok = num % 10;
+Console.WriteLine(ostatok);
+//string num = Console.ReadLine(); легкое решение
+//System.Console.WriteLine(num[2]);
