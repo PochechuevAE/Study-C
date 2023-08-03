e2f3148c878b8cdd3358e200e9ac2c4eed46a460
Pochechuev <trakturus@list.ru>
2023-08-03 21:12:22 +0300

    Seminar 2 Example_10

--- Example_10/Program.cs
+++ Example_10/Program.cs
@@ -4,34 +4,41 @@ int day = int.Parse(Console.ReadLine());
 if (day == 1)
 {
     Console.WriteLine("Сегодня Понедельник"); //Но можно и через свич кейс
+    return;
 }
 
 if (day == 2)
 {
     Console.WriteLine("Сегодня Вторник");
+    return;
 }
 
 if (day == 3)
 {
     Console.WriteLine("Сегодня Среда");
+    return;
 }
 
 if (day == 4)
 {
     Console.WriteLine("Сегодня Четверг");
+    return;
 }
 
 if (day == 5)
 {
     Console.WriteLine("Сегодня Пятница");
+    return;
 }
 
 if (day == 6)
 {
     Console.WriteLine("Сегодня Суббота");
+    return;
 }
 
 if (day == 7)
 {
     Console.WriteLine("Сегодня Воскресенье");
+    return;
 }
