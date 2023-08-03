cca374cd7f7dac3b1176c4a487b0b96502d9cc88
Pochechuev <trakturus@list.ru>
2023-08-03 20:23:11 +0300

    Seminar 2 Example_009

--- Example_009/Program.cs
+++ Example_009/Program.cs
@@ -1,1 +1,6 @@
-﻿// 
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+Console.WriteLine("Введите число ");
+string numStr = 12 //Console.ReadLine();
+int num = int.Parse(numStr); // int.Parse преобразует строки в числа можно int.Parse(Console.ReadLine());
+int result = num * num;
+Console.WriteLine("Квадрат числа равняется " + result);
