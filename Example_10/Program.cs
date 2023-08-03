647e75f3b9d4a9f9416ba75a0aa6a1c03321c16e
Pochechuev <trakturus@list.ru>
2023-08-03 21:04:19 +0300

    Seminar 2 Example_10

---
+++ Example_10/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+Console.WriteLine("Введите номер дня недели от 1 до 7 включительно: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("Сегодня Понедельник");
+}
+
+if (day == 2)
+{
+    Console.WriteLine("Сегодня Вторник");
+}
+
+if (day == 3)
+{
+    Console.WriteLine("Сегодня Среда");
+}
+
+if (day == 4)
+{
+    Console.WriteLine("Сегодня Четверг");
+}
+
+if (day == 5)
+{
+    Console.WriteLine("Сегодня Пятница");
+}
+
+if (day == 6)
+{
+    Console.WriteLine("Сегодня Суббота");
+}
+
+if (day == 7)
+{
+    Console.WriteLine("Сегодня Воскресенье");
+}
+
