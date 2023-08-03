e9dc6e3011a656c6a06fd2905725cb592fadb1fb
Pochechuev <trakturus@list.ru>
2023-08-03 21:29:02 +0300

    Seminar 2 Example_012

--- Example_012/Program.cs
+++ Example_012/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Введите число: ");
 int Num = int.Parse(Console.ReadLine());
 int otrNum = - Num;
-while (otrNum != Num)
+while (otrNum <= Num)
 {
     System.Console.WriteLine(otrNum);
     otrNum++;
