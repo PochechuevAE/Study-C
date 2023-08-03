eaaeb0353b0ac12a3ae70fad4e73bda2f90f23fd
Pochechuev <trakturus@list.ru>
2023-08-03 21:27:45 +0300

    Seminar 2 Example_012

---
+++ Example_012/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine());
+int otrNum = - Num;
+while (otrNum != Num)
+{
+    System.Console.WriteLine(otrNum);
+    otrNum++;
+}
