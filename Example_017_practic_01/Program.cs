766f2d9bf34f52e777166ece81f3333acf1494aa
Pochechuev <trakturus@list.ru>
2023-08-04 20:25:15 +0300

    add Example_017_practic_01

---
+++ Example_017_practic_01/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int randomNumber = new Random().Next(10, 100);
+int num1 = randomNumber % 10;
+int num2 = randomNumber / 10;
+System.Console.WriteLine($"Выбрано число: {randomNumber}"); //Строковая интерполяция удобно :)
+System.Console.Write("Максимальная цифра: ");
+System.Console.WriteLine(num1 > num2 ? num1 : num2);
