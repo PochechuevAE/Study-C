3e1db985a25faeaf74c9897a2f728ae8afda93a1
Pochechuev <trakturus@list.ru>
2023-08-25 20:30:59 +0300

    Do task1

--- Seminar_24_08_23/Task1/Program.cs
+++ Seminar_24_08_23/Task1/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
-
+int res = GetSumFrom1ToN(10);
+System.Console.WriteLine(res);
 int GetSumFrom1ToN(int a)
 {
   int result = 0;
   int count = 0;
-  for (int i = 0; count <= a; i++)
+  while (count <= a)
   {
-
+    result += count;
+    count++;
   }
+
+  return result;
 }
 
