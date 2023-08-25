3e0e824dafc8ecf41d1e13ebdaa61ac4538e022b
Pochechuev <trakturus@list.ru>
2023-08-25 20:46:22 +0300

    Wrote task2

--- Seminar_24_08_23/Task2/Program.cs
+++ Seminar_24_08_23/Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+System.Console.WriteLine(GivLenghthNum(23523));
+
+int GivLenghthNum(int num)
+{
+    int Lenght = default;
+    
+    while (num > 0) 
+    {
+        Lenght++;
+        num /= 10;       
+    }
+
+    return Lenght;
+}
