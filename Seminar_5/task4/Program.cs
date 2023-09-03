1bf2508a8fbe563d7a2e066a5ff994e8631fd69c
Pochechuev <trakturus@list.ru>
2023-09-03 16:54:14 +0300

    Wrote task4 from Seminar_5

--- Seminar_5/task4/Program.cs
+++ Seminar_5/task4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+int [] arr = GenerateArray(123);
+PrintArray(arr); 
+System.Console.WriteLine($"Кол-во элементов массива в данном диапазоне = {FindNumInInterval(arr)}");
+
+int FindNumInInterval(int[] array) // Находит кол-во элементов в заданом интервале [10; 99]
+{
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 && array[i] <= 99)
+        count++;
+
+    }
+    return count;
+}
+
+int [] GenerateArray(int Length) 
+{
+    int [] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 1000);
+    }
+
+    return result;
+} 
+
+void PrintArray(int [] array)
+{
+    foreach (int i in array)
+    {
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+}
