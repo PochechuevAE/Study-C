72c731d89eecdb453cbfb07b1aae7d9196ab23ef
Pochechuev <trakturus@list.ru>
2023-09-03 17:09:00 +0300

    Wrote task5 to Seminar_5

---
+++ Seminar_5/task5/Program.cs
@@ -0,0 +1,51 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+ 
+int[] arr = GenerateArray(5);
+PrintArray(arr); 
+int[] newArray = FindSumOfPairs(arr);
+PrintArray(newArray);
+
+int[] FindSumOfPairs(int[] array) // Находит пары элементов и умножает их]
+{
+    int newSize = array.Length / 2;
+
+    if(array.Length % 2 == 1)
+    {
+        newSize++;
+    }
+
+    int[] result = new int[newSize];
+
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+
+   if(array.Length % 2 == 1)
+    {
+        result[newSize - 1] = array[array.Length / 2];
+    }
+    
+    return result;
+}
+
+int [] GenerateArray(int Length) 
+{
+    int [] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 11);
+    }
+
+    return result;
+} 
+
+void PrintArray(int [] array)
+{
+    foreach (int i in array)
+    {
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+}
