829fbdcdbacec0af3e75ef385c764350f2aba278
Pochechuev <trakturus@list.ru>
2023-09-03 15:13:18 +0300

    add task1 to Seminar_5

---
+++ Seminar_5/task1/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задайте массив из 12 элементов, заполненный
+//случайными числами из промежутка [-9, 9]. Найдите сумму
+//отрицательных и положительных элементов массива
+int [] arr = GenerateArray(12);
+PrintArray(arr);
+System.Console.WriteLine($"Сумма положительных элементов массива равна {GetSumMoreZero(arr)}");
+System.Console.WriteLine($"Сумма положительных элементов массива равна {GetSumLessZero(arr)}");
+
+int [] GenerateArray(int Length) 
+{
+    int [] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(-9, 9);
+    }
+
+    return result;
+} 
+
+void PrintArray(int [] array)
+{
+    foreach (int i in array)
+    {
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+}
+
+int GetSumMoreZero(int [] array) 
+{
+    int result = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0)
+        {
+            result += array[i];
+        }
+    } 
+
+    return result;
+}
+
+int GetSumLessZero(int [] array) 
+{
+    int result = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            result += array[i];
+        }
+    } 
+
+    return result;
+}
