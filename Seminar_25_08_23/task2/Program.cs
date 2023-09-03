3491fb5d8a546646e5b614188f9754bbe8defbaf
Pochechuev <trakturus@list.ru>
2023-09-03 14:48:41 +0300

    Add task 2 in Seminar_25_08_23

---
+++ Seminar_25_08_23/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+System.Console.WriteLine($"{GetLengthNum(48736)}");
+
+int GetLengthNum(int num)
+{
+    int len = default;
+    
+    while (num > 0)
+    {
+        len++;
+        num /= 10;
+    }
+
+    return len;
+}
