b1fddd0f5366e5b3526d3610cc86b877edd3519b
Pochechuev <trakturus@list.ru>
2023-09-03 15:50:58 +0300

    wrote task2 to Seminar_5

--- Seminar_5/task2/Program.cs
+++ Seminar_5/task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+int [] arr = GenerateArray(5);
+PrintArray(arr);
+PrintArray(ReplaseNum(arr));
+
+int [] ReplaseNum(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] *= -1;
+    return array;
+} 
+
+int [] GenerateArray(int Length) 
+{
+    int [] result = new int[Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(-19, 20);
+    }
+
+    return result;
+} 
+
+void PrintArray(int [] array)
+{
+    foreach (int i in array)
+    {
+        System.Console.Write($"{i} ");
+    }
+    System.Console.WriteLine();
+}
