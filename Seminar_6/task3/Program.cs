a3421abb4727055864c3f79f33b0be20dc3247e8
Pochechuev <trakturus@list.ru>
2023-09-04 18:10:07 +0300

    task3

--- Seminar_6/task3/Program.cs
+++ Seminar_6/task3/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Работа с классом List
+
+var persons = new List<Person>{ // Создаем базу
+    new Person{
+        Id = 1,
+        Name = "John",
+    },
+    new Person{
+        Id = 2,
+        Name = "Alexander",
+    },
+    new Person{
+        Id = 3,
+        Name = "Natan",
+    },
+    new Person{
+        Id = 4,
+        Name = "Igor",
+    },
+};
+
+System.Console.WriteLine(persons.Where(p => p.Id == 1).FirstOrDefault()?.Name); // Обращаемся к базе 1 элем базы
+System.Console.WriteLine(persons[1].Name); // Обращаемся к базе к 1 элем массива
+
+
+public class Person //записываем свойства
+{
+    public int Id { get; set;}
+    public string Name { get; set; }
+}
